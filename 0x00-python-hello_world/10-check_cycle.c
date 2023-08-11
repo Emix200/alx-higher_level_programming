234ad670f473fc81c6f2312c6ba398bf31e24c26
emix200 <aleajiemix@gmail.com>
2023-08-11 11:34:32 -0700

    hello python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that checks singly linked lists
+ * @list: pointer to initial node
+ * Return: success if circle else false
+ */
+int check_cycle(listint_t *list);
+{
+	listint_t *current, *check;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	current = list;
+	check = current->next;
+
+	while (current != NULL && check->next != NULL
+			&& check->next != NULL)
+	{
+		if (current == check)
+			return (1);
+		current = current->next;
+		check = check->next->next;
+	}
+	return (0);
+}
