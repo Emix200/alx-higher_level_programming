c17b30968b39e9e790c392ecd5430cdb637df824
emix200 <aleajiemix@gmail.com>
2023-08-21 07:10:59 -0700

    import project updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     """Print the addition of all arguements."""
     import sys
     total = 0
-for s in range(len(sys.argv) - 1):
-    total += int(sys.arg[s + 1])
+for i in range(len(sys.argv) - 1):
+    total += int(sys.arg[i + 1])
 print("{}".format(total))
